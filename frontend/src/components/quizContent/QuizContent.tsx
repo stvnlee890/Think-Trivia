83734a577cdb0144a57420771698bbe46ea06b35
stvnlee890 <stvnlee890@gmail.com>
2023-07-02 16:09:22 -0700

    Add basic layout styling and randomize quiz answers

--- frontend/src/components/quizContent/QuizContent.tsx
+++ frontend/src/components/quizContent/QuizContent.tsx
@@ -1,3 +1,4 @@
+import "./quizContent.css";
 import { useState } from "react";
 import { QuizItem } from "../quizPage/QuizPage";
 
@@ -6,19 +7,40 @@ export interface IProps {
 }
 
 export default function QuizContent({ quiz }: IProps) {
-  const [count, setCount] = useState<number>(0);
+  console.log(quiz);
+  const [questionIndex, setQuestionIndex] = useState<number>(0);
+  const currentQuestion = quiz[questionIndex]
+
   const handleCount = () => {
-    if (count < 9) {
-        setCount((count) => count + 1)
-    } 
+    if (questionIndex < 9) {
+      setQuestionIndex((questionIndex) => questionIndex + 1);
+    }
   };
 
+  const shuffle = (array: string[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  const answers = shuffle([...currentQuestion.incorrectAnswers, currentQuestion.correctAnswer])
+
   return (
-    <div>
-      <h1>Quiz Page</h1>
-      <p>{count} / {quiz.length}</p>
-      <p>{quiz[count].question.text}</p>
+    <section className="quiz-content container">
+      <p>
+        {questionIndex + 1} / {quiz.length}
+      </p>
+      <div className="quiz-content wrapper">
+        <p>{currentQuestion.question.text}</p>
+      </div>
+      <div className="answer">
+        { answers.map((ele, idx) => (
+            <p key={idx}>{ele}</p>
+        )) }
+      </div>
       <button onClick={handleCount}>Next</button>
-    </div>
+    </section>
   );
 }
