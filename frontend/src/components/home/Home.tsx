b8e732c33a75b491937b8f5c2fd0efb22cf6c9ef
stvnlee890 <stvnlee890@gmail.com>
2023-07-10 15:56:00 -0700

    Add flex to align items evenly. So far looking good

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,20 +1,22 @@
 import "./home.css";
 import { useNavigate } from "react-router-dom";
-import ideas from '../../assets/homeIcons/ideas.png'
-import quiz from '../../assets/homeIcons/quiz.png'
+import ideas from "../../assets/homeIcons/ideas.png";
+import quiz from "../../assets/homeIcons/quiz.png";
 
 export default function Home() {
   const navigate = useNavigate();
   return (
     <div className="home-container">
-      <h1 className="home-header">Think Trivia</h1>
-      <p className="home-text">
-        Unleash your curiosity with our random <br /> trivia app! Engage in
-        endless brain-teasing <br /> fun as you tackle unpredictable questions{" "}
-        <br /> that will keep you guessing.
-      </p>
+      <div className="home-text-container">
+        <h1 className="home-header">Think Trivia</h1>
+        <p className="home-text">
+          Unleash your curiosity with our random <br /> trivia app! Engage in
+          endless brain-teasing <br /> fun as you tackle unpredictable questions{" "}
+          <br /> that will keep you guessing.
+        </p>
+      </div>
       <div className="icon-container">
-        <img className="icons ideas" src={ideas} ></img>
+        <img className="icons ideas" src={ideas}></img>
         <img className="icons quiz" src={quiz}></img>
       </div>
       <button onClick={() => navigate("/quiz")}>Start Playing</button>
