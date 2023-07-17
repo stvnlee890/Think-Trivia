ce5686af74a2b6a15fc2305f8609e2b5a19cbdae
stvnlee890 <stvnlee890@gmail.com>
2023-07-17 16:49:04 -0700

    Work on loading animation

--- frontend/src/components/quizPage/QuizPage.tsx
+++ frontend/src/components/quizPage/QuizPage.tsx
@@ -1,7 +1,8 @@
 import "./quizPage.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { TriviaApiService } from "../../services/triviaApiService";
+import { TriviaApiService } from "../../services/apiService/triviaApiService";
+import { dotAnimation } from "../../services/animationService/animationHelper";
 import QuizContent from "../quizContent/QuizContent";
 
 export interface QuizItem {
@@ -20,23 +21,38 @@ export interface QuizItem {
 export default function QuizPage() {
   const navigate = useNavigate();
   const baseUrl = "https://the-trivia-api.com/v2";
+  const dotRef = useRef<HTMLDivElement>(null);
   const triviaApiService = new TriviaApiService(baseUrl);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const [quiz, setQuiz] = useState<QuizItem[]>([]);
   useEffect(() => {
+    if (dotRef.current) {
+      dotAnimation(dotRef.current.children);
+    }
     triviaApiService.getRandomQuestions().then((res) => setQuiz(res));
     setTimeout(() => {
       setIsLoading(false);
-    }, 1000);
+    }, 2000);
   }, []);
 
   return (
     <div className="quizpage-container">
       <p onClick={() => navigate("/")}>HOME</p>
       <div className="quizpage-wrapper">
-        {(!isLoading && quiz.length > 0) && <QuizContent quiz={quiz} />}
-        {isLoading && <h1 className="loading">Loading your quiz</h1>}
+        {!isLoading && quiz.length > 0 && <QuizContent quiz={quiz} />}
+        {isLoading && (
+          <>
+            <div className="loading-container">
+              <h1>Loading</h1>
+              <div ref={dotRef} className="loading-dots">
+                <div className="loading-dot 1"></div>
+                <div className="loading-dot 2"></div>
+                <div className="loading-dot 3"></div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
